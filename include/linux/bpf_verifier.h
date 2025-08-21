67943775ecc8e3f05fe336e562bfa7f47cc8bd76
chenlei <chenlei.oerv@isrc.iscas.ac.cn>
2025-08-21 15:28:15 +0800

    bpf: Implement BPF exceptions
    mainline inclusion
    from mainline-v6.7-rc1
    commit f18b03fabaa9b7c80e80b72a621f481f0d706ae0
    category: feature
    bugzilla: https://github.com/RVCK-Project/rvck-olk/issues/110
    Reference: https://lore.kernel.org/r/20230912233214.1518551-5-memxor@gmail.com
    
    --------------------------------
    
    bpf: Implement BPF exceptions
    
    This patch implements BPF exceptions, and introduces a bpf_throw kfunc
    to allow programs to throw exceptions during their execution at runtime.
    A bpf_throw invocation is treated as an immediate termination of the
    program, returning back to its caller within the kernel, unwinding all
    stack frames.
    
    This allows the program to simplify its implementation, by testing for
    runtime conditions which the verifier has no visibility into, and assert
    that they are true. In case they are not, the program can simply throw
    an exception from the other branch.
    
    BPF exceptions are explicitly *NOT* an unlikely slowpath error handling
    primitive, and this objective has guided design choices of the
    implementation of the them within the kernel (with the bulk of the cost
    for unwinding the stack offloaded to the bpf_throw kfunc).
    
    The implementation of this mechanism requires use of add_hidden_subprog
    mechanism introduced in the previous patch, which generates a couple of
    instructions to move R1 to R0 and exit. The JIT then rewrites the
    prologue of this subprog to take the stack pointer and frame pointer as
    inputs and reset the stack frame, popping all callee-saved registers
    saved by the main subprog. The bpf_throw function then walks the stack
    at runtime, and invokes this exception subprog with the stack and frame
    pointers as parameters.
    
    Reviewers must take note that currently the main program is made to save
    all callee-saved registers on x86_64 during entry into the program. This
    is because we must do an equivalent of a lightweight context switch when
    unwinding the stack, therefore we need the callee-saved registers of the
    caller of the BPF program to be able to return with a sane state.
    
    Note that we have to additionally handle r12, even though it is not used
    by the program, because when throwing the exception the program makes an
    entry into the kernel which could clobber r12 after saving it on the
    stack. To be able to preserve the value we received on program entry, we
    push r12 and restore it from the generated subprogram when unwinding the
    stack.
    
    For now, bpf_throw invocation fails when lingering resources or locks
    exist in that path of the program. In a future followup, bpf_throw will
    be extended to perform frame-by-frame unwinding to release lingering
    resources for each stack frame, removing this limitation.
    
    Signed-off-by: Kumar Kartikeya Dwivedi <memxor@gmail.com>
    Link: https://lore.kernel.org/r/20230912233214.1518551-5-memxor@gmail.com
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Signed-off-by: chenlei <chenlei.oerv@isrc.iscas.ac.cn>

--- include/linux/bpf_verifier.h
+++ include/linux/bpf_verifier.h
@@ -637,6 +637,7 @@ struct bpf_subprog_info {
 	bool has_tail_call;
 	bool tail_call_reachable;
 	bool has_ld_abs;
+	bool is_cb;
 	bool is_async_cb;
 	KABI_FILL_HOLE(bool changes_pkt_data)
 
@@ -644,6 +645,7 @@ struct bpf_subprog_info {
 	KABI_RESERVE(2)
 	KABI_RESERVE(3)
 	KABI_RESERVE(4)
+	bool is_exception_cb;
 };
 
 struct bpf_verifier_env;
@@ -691,6 +693,7 @@ struct bpf_verifier_env {
 	u32 used_btf_cnt;		/* number of used BTF objects */
 	u32 id_gen;			/* used to generate unique reg IDs */
 	u32 hidden_subprog_cnt;		/* number of hidden subprogs */
+	int exception_callback_subprog;
 	bool explore_alu_limits;
 	bool allow_ptr_leaks;
 	bool allow_uninit_stack;
@@ -698,6 +701,7 @@ struct bpf_verifier_env {
 	bool bypass_spec_v1;
 	bool bypass_spec_v4;
 	bool seen_direct_write;
+	bool seen_exception;
 	struct bpf_insn_aux_data *insn_aux_data; /* array of per-insn state */
 	const struct bpf_line_info *prev_linfo;
 	struct bpf_verifier_log log;
